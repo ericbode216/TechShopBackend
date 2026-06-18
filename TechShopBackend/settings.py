e3f87b018340fa3ab38e640f1870aaa578756cb0
Eric Bode <ericbode216@gmail.com>
2026-06-18 09:32:48 -0400

    changed allowed hosts

--- TechShopBackend/settings.py
+++ TechShopBackend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.173']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
